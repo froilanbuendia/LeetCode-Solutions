67a48a5bfbf9043dc77feac2afa293d96637e369
froilanbuendia <80615020+froilanbuendia@users.noreply.github.com>
2022-09-27 20:02:19 -0700

    Time: 56 ms (86.71%), Space: 14.5 MB (67.08%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        for letter in letters:
+            if s.count(letter) != t.count(letter):
+                return False
+        return True
