8a36bdbc47ef3b992cb5cb38c94fdabf3aa39919
froilanbuendia <80615020+froilanbuendia@users.noreply.github.com>
2022-10-19 15:09:54 -0700

    Time: 714 ms (81.35%), Space: 13.9 MB (57.53%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,27 +1,39 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        #start from the middle and expand outwards
-        #check for both odd and even bounds
+        # start from the middle and expand outwards
+        if len(s) == 1:
+            return s[0]
+        elif len(s) == 2:
+            if s[0] == s[1]:
+                return s[0] + s[1]
+            else:
+                return s[0]
         res = ''
-        s_idx = 0
+        s_idx = 0 # start index
         res_l = 0
-        for i in range(len(s)):
+        for i in range(len(s)): # goes through the string
+            # odd length
             l = i
             r = i
+            # checks if left and right index are equal and in bounds
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if(r - l + 1) > res_l:
                     s_idx = l
                     res_l = r - l + 1
+                # decrements and increments left and right
                 l -= 1
                 r += 1
+            # even length
             l = i
             r = i + 1
+            # checks if left and right index are equal and in bounds
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if(r - l + 1) > res_l:
                     s_idx = l
                     res_l = r - l + 1
+                # decrements and increments left and right
                 l -= 1
                 r += 1
+        # creates the string to return
         res = s[s_idx:s_idx + res_l]
         return res
-
