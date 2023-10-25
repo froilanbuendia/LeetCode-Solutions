976c0d299192a710a4967a7938e55be0384a98ae
froilanbuendia <froilangbuendia@gmail.com>
2023-10-25 10:40:36 -0700

    contains duplicate comments

--- 217-contains-duplicate.py
+++ 217-contains-duplicate.py
@@ -1,9 +1,14 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # creates a set
         hashset = set()
+
+        # loops through nums list
         for i in nums:
+            # checks if i already in hashset
             if i in hashset:
                 return True
+            # adds i to hashset
             hashset.add(i)
         return False
                 
